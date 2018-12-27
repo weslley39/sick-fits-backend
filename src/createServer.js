dc63fa568ab61b2f086864b7a110b4f1e74d6802
Weslley Neri <weslley39@gmail.com>
2018-12-27 16:03:40 -0200

    deploy

--- src/createServer.js
+++ src/createServer.js
@@ -4,7 +4,6 @@ const Query  = require('./resolvers/Query');
 const db = require('./db');
 
 // Create GraphQL Yoga Server
-
 function createServer() {
   return new GraphQLServer({
     typeDefs: 'src/schema.graphql',
