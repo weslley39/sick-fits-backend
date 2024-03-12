56b0e66e112c5c1e75d8954b05d33658455f9441
Weslley Neri <weslley39@gmail.com>
2024-03-12 10:50:24 -0300

    port 3000 as default

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ server.start(
       credentials: true,
       origin: process.env.FRONTEND_URL,
     },
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 3000,
   },
   deets => {
     console.log(`The magic happens on port http://localhost:${deets.port}`);
